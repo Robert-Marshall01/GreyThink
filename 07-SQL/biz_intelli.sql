ad1b5f066bbaf0a550b9592addb7a005bc606995
Robert-Marshall01 <RobertCMarshall2007@outlook.com>
2025-10-05 16:56:00 -0600

    Create biz_intelli.sql

---
+++ 07-SQL/biz_intelli.sql
@@ -0,0 +1,19 @@
+/*
+  CITATION for instructions: https://www.youtube.com/watch?v=9ADd-_mM5Dw
+  VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+  To install:
+  1. Go to Visual Code.
+  2. Download the "SQLTools" extension.
+  3. Download another extension that starts with "SQLTools" following with the desired 
+  RDBMS (Relational DataBase Management System) such as MySQL.
+  4. Go to "Add New Connection" under the new cylinder DB button added to your left-hand bar.
+  5. Select your desired RDBMS.
+  6. Input your connection name and username.
+  7. Add an additional '0' to the given port number to make it '33060.'
+  8. Change password to plaintext in settings and type in your password.
+  9. Change authentication protocal to "xprotocol."
+  10. Specify a connection time-out setting (e.x. 30).
+  11. Save connection.
+  12. Click on the "plug icon" to connect to the database, and be in your database.
+  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+*/
