f08c64bf1a22db1d03aa98755b9897a0db24bcb1
Robert-Marshall01 <RobertCMarshall2007@outlook.com>
2025-10-05 18:23:43 -0600

    Provided instructions.

--- 07-SQL/biz_intelli.sql
+++ 07-SQL/biz_intelli.sql
@@ -8,12 +8,21 @@
   RDBMS (Relational DataBase Management System) such as MySQL.
   4. Go to "Add New Connection" under the new cylinder DB button added to your left-hand bar.
   5. Select your desired RDBMS.
-  6. Input your connection name and username.
+  6. Input your connection name, username, and database.
   7. Add an additional '0' to the given port number to make it '33060.'
   8. Change password to plaintext in settings and type in your password.
   9. Change authentication protocal to "xprotocol."
   10. Specify a connection time-out setting (e.x. 30).
-  11. Save connection.
-  12. Click on the "plug icon" to connect to the database, and be in your database.
+  IMPORTANT: steps 11 - 14 are MySQL specific.
+  ------------------------------------------------------------------------------------------------------
+  11. Go onto https://dev.mysql.com/downloads/file/?id=544662 to download the dependencies.
+  12. Follow the wizard.
+  13. Go onto your PowerShell, and type:
+  & "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" -u YOUR_USERNAME -p
+  14. Once you have "mysql>" on the left as your input, type the following command:
+  CREATE DATABASE your_database;
+  ------------------------------------------------------------------------------------------------------
+  15. Save connection.
+  16. Click on the "plug icon" to connect to the database, and be in your database.
   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 */
