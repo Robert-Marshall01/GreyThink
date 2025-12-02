ec1917306547f33499800e8b167457345a389b03
Robert-Marshall01 <RobertCMarshall2007@outlook.com>
2025-12-01 20:37:26 -0700

    Exclude lock files from line count analysis

--- CLI_Line_Count.sh
+++ CLI_Line_Count.sh
@@ -1,2 +1,2 @@
 #This is a Bash one-liner that analyzes the line count of the GreyThink repo.
-git ls-files | grep -vE '\.(txt|md|png|csv)$' | xargs wc -l
+git ls-files | grep -vE '\.(txt|md|png|csv|lock)$' | xargs wc -l
